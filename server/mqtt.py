3a1e123053b23f6e3e2cb0bceabfb552a904e5fd
david <david@bigserver>
2018-12-16 20:59:26 +0100

    make json decoding optional

--- server/mqtt.py
+++ server/mqtt.py
@@ -2,7 +2,7 @@ import json
 import paho.mqtt.client as mqtt
 
 class Mqtt:
-    def __init__(self, host, port, topics, cb_list):
+    def __init__(self, host, port, topics, cb_list, _json=True):
         self.mqttc = mqtt.Client()
         self.mqttc.connect(host, port=port)
         for topic in topics:
@@ -10,12 +10,17 @@ class Mqtt:
 
         self.mqttc.on_message = self.on_message
         self.cb_list = cb_list
+        self.json = _json
 
     def on_message(self, client, userdata, message):
         msg = message.payload.decode('ascii')
         try:
-            j = json.loads(message.payload.decode('ascii'))
+            if self.json:
+                j = json.loads(message.payload.decode('ascii'))
+            else:
+                j = message.payload.decode('ascii')
         except json.decoder.JSONDecodeError:
+            print("Failed to decode JSON")
             return
         for cb in self.cb_list:
             cb(message.topic, j)
