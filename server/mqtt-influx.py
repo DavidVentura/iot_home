adf453dce9f0fb53fa8521707d764df7f1ac277a
david <davidventura27@gmail.com>
2019-08-11 13:26:01 +0200

    add printer parsing

--- server/mqtt-influx.py
+++ server/mqtt-influx.py
@@ -7,12 +7,12 @@ import datetime
 from mqtt import Mqtt
 
 GRAFANA_URL = "http://db.labs:8086/write?db=sensordata"
-
+custom_parsing = { 'printer': printer_parsing }
 
 def setup():
     host = 'iot'
     port = 1883
-    topics = ["TEMP/TEMPSENSOR", "HUM/TEMPSENSOR", "HUM/#", "TEMP/#", "NIGHTLAMP2/state", "KINDLE/battery/state", "KINDLE/percentage/state", "phones/#"]
+    topics = ["TEMP/TEMPSENSOR", "HUM/TEMPSENSOR", "HUM/#", "TEMP/#", "NIGHTLAMP2/state", "KINDLE/battery/state", "KINDLE/percentage/state", "phones/#", "printer/#"]
     # CONFIG
     return Mqtt(host, port, topics, [to_influx], _json=False)
 
@@ -27,19 +27,40 @@ def main():
 
 def to_influx(topic, value):
     print(datetime.datetime.now(), topic, value)
+    _type = topic.split('/')[0]
+    if _type in custom_parsing:
+        custom_parsing[_type](topic, value)
+    else:
+        default_parsing(topic, value)
 
+def post_to_grafana(data):
+        try:
+            response = requests.post(GRAFANA_URL, data=_data, timeout=2)
+            if not response.ok:
+                print(_data)
+                print(response)
+                print(response.text)
+        except Exception as e:
+            print(e)
+
+
+def printer_parsing(topic, value):
+    _type = topic.split('/')[0]
+    if topic == "printer/JOB_STATUS":
+        _data = 'printer_job_status percentage=%f' % (float(value))
+        post_to_grafana(_data)
+    elif topic == "printer/TEMP":
+        nozzle, bed = value.split(',')
+        for sensor, current, target in [['nozzle']+nozzle.split('/'), ['bed']+bed.split('/')]
+            _data = 'printer_temp,sensor="%s" current=%f,target=%f' % (sensor, float(current), float(target))
+            post_to_grafana(_data)
+
+
+def default_parsing(topic, value):
     _type = topic.split('/')[0]
     sensor = topic.split('/')[1]
     _data = '%s,sensor="%s" value=%f' % (_type, sensor, float(value))
-    try:
-        response = requests.post(GRAFANA_URL, data=_data, timeout=2)
-        if not response.ok:
-            print(_data)
-            print(response)
-            print(response.text)
-    except Exception as e:
-        print(e)
-
+    post_to_grafana(_data)
     
 if __name__ == '__main__':
     main()
