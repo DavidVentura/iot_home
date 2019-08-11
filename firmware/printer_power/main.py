912a45f241c2f4a7df3d4b65abb2682aad975101
david <davidventura27@gmail.com>
2019-08-11 11:35:30 +0200

    update printer code to send emergency stop on poweroff

--- firmware/printer_power/main.py
+++ firmware/printer_power/main.py
@@ -4,6 +4,7 @@ from machine import Pin
 CLIENT_ID = common.get_client_id()
 SUBTOPIC = b"%s/set" % CLIENT_ID
 PUBTOPIC = b"%s/state" % CLIENT_ID
+EMERGENCY_STOP_PUBTOPIC = b"printer/commands"
 
 button = Pin(0, Pin.IN)
 led = Pin(13, Pin.OUT)
@@ -27,7 +28,10 @@ def sub_cb(topic, msg):
 @common.debounce(250)
 def handle_button(pin):
     common.log('Button was pressed')
-    set_pin(relay, not relay())
+    new_state = not relay()
+    set_pin(relay, new_state)
+    if new_state == False:
+        common.publish(EMERGENCY_STOP_PUBTOPIC, "stop")
 
 def setup():
     button.irq(handler=handle_button, trigger=Pin.IRQ_RISING)
